0b90c5042d790c7a04b8035795f4572146306ee4
Julian Horn <julian.horn@aoe.com>
2016-07-16 15:33:26 +0200

    [Chrome Plugin] Enables the functionality to save the last request as mock (the method in the AdminServer is still buggy, though) . Adds another pane for activating and deactivating mocks.

--- chrome_plugin/background.js
+++ chrome_plugin/background.js
@@ -1,15 +1,6 @@
 // Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
-  
-    //chrome.tabs.executeScript({
-    //  code: 'document.body.style.backgroundColor="red"'
-  });
   
 chrome.runtime.onConnect.addListener(function(port){
      if(port.name === "GetPossibleMockList"){
@@ -24,14 +15,14 @@ chrome.runtime.onConnect.addListener(function(port){
 });
 
 chrome.runtime.onConnect.addListener(function(port){
-     if(port.name === "SaveToAvailableMocks"){
+     if(port.name === "SaveLastRequestToMocks"){
         // Send request and list
         // This here must be changed so that the correct endpoint will be called
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://www.localhost:8001/api/available-requests", false);
+        xhr.open("GET", "http://www.localhost:8001/api/save-last-request", false);
         xhr.send();
         var result = xhr.responseText;
-        var jsonResult = JSON.parse(result)
-        port.postMessage({result: "Added to available mocks!"});   
+        //var jsonResult = JSON.parse(result)
+        port.postMessage({result: result});   
      }
 });
--- chrome_plugin/panelCreation/createPanel.js
+++ chrome_plugin/panelCreation/createPanel.js
@@ -2,5 +2,7 @@
 // chrome.devtools.*
 // chrome.extension.*
 
-// Create a tab in the devtools area
+// Create a tab in the devtools area for creating mocks from requests
 chrome.devtools.panels.create("Create Mocks", "images/icon.png", "createMockPanel/panel.html", function(panel) {});
+// Create a tab in the devtools area for activation and deactivating mocks
+chrome.devtools.panels.create("Manage Mocks", "images/icon.png", "manageMockPanel/manageMockPanel.html", function(panel) {});
