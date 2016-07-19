2b9563f706b5ef6ea4324965fe37e62e71f3b649
Marc Hammerton <marc.hammerton@aoe.com>
2016-07-19 21:45:42 +0200

    WIP: started splitting admin requests

--- admin/routes/requests.js
+++ admin/routes/requests.js
@@ -3,44 +3,6 @@ var router = express.Router();
 var config = require('config');
 var fs = require('fs');
 
-// save last request
-router.get('/save-last-request', function (req, res) {
-	fs.readFile("./logs/requests.log", "utf-8", function (err, data) {
-
-		// prepare data
-		var rawRequests = data.split('\n');
-		rawRequests.pop();
-
-		var requests = [];
-		rawRequests.forEach(function (rawRequest) {
-			var request = JSON.parse(rawRequest);
-			requests.push({
-				fileName: request.fileName,
-				request: request.request,
-				response: request.response
-			});
-		});
-
-		// save last response
-		if (typeof err === 'undefined' || err === null) {
-			var lastRequest = requests[requests.length - 1];
-			fs.writeFile('./mocks-enabled' + lastRequest.fileName, lastRequest.response, function (err) {
-				if (err) {
-					res.statusCode = 500;
-					res.json({message: 'ups! something went wrong: ' + err});
-				} else {
-					res.json({message: 'last request saved with filename and enabled: ' + lastRequest.fileName});
-				}
-			});
-
-		} else {
-			res.statusCode = 500;
-			res.json({message: 'ups! something went wrong: ' + err});
-		}
-
-	});
-});
-
 // logged requests
 router.get('/', function (req, res) {
 	fs.readFile(config.get('logging').get('file'), "utf-8", function (err, data) {
