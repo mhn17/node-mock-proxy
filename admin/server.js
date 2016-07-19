894b8d133daf8503ca01da43910c2899edc3071a
Marc Hammerton <marc.hammerton@aoe.com>
2016-07-19 19:45:20 +0200

    start refactoring admin api

--- admin/server.js
+++ admin/server.js
@@ -1,8 +1,10 @@
 var express     = require('express');
-var router      = express.Router();
-var bodyParser  = require('body-parser');
-var config      = require('config');
-var fs          = require('fs');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var config = require('config');
+
+// routes
+var requestRoutes = require('./routes/requests.js');
 
 var AdminServer = function() {
     this.app = express();
@@ -20,7 +22,6 @@ AdminServer.prototype.start = function() {
 
     // routes
     this.setUpRoutes();
-    this.app.use('/api', router);
 
     // start server
     this.app.listen(this.adminConfig.get('port'), function () {
@@ -29,79 +30,9 @@ AdminServer.prototype.start = function() {
 };
 
 AdminServer.prototype.setUpRoutes = function() {
-    // root URL
-    router.get('/', function(req, res) {
-        res.json({ message: 'hooray! welcome to our api!' });
-    });
-
-	// save last request
-	router.get('/save-last-request', function(req, res) {
-		fs.readFile("./logs/requests.log", "utf-8", function(err, data) {
-
-			// prepare data
-			var rawRequests = data.split('\n');
-			rawRequests.pop();
-
-			var requests = [];
-			rawRequests.forEach(function(rawRequest) {
-				var request = JSON.parse(rawRequest);
-				requests.push({
-					fileName: request.fileName,
-					request: request.request,
-					response: request.response
-				});
-			});
-
-			// save last response
-			if(typeof err === 'undefined' || err === null) {
-				var lastRequest = requests[requests.length-1];
-				fs.writeFile('./mocks-enabled' + lastRequest.fileName, lastRequest.response, function(err) {
-					if (err) {
-						res.statusCode = 500;
-						res.json({message: 'ups! something went wrong: ' + err});
-					} else {
-						res.json({message: 'last request saved with filename and enabled: ' + lastRequest.fileName});
-					}
-				});
-
-			}
-			else {
-				res.statusCode = 500;
-				res.json({message: 'ups! something went wrong: ' + err});
-			}
-
-		});
-	});
-
-    // logged requests
-    router.get('/available-requests', function(req, res) {
-        fs.readFile("./logs/requests.log", "utf-8", function(err, data) {
-
-            // prepare data
-            var rawRequests = data.split('\n');
-            rawRequests.pop();
-
-            var requests = [];
-            rawRequests.forEach(function(rawRequest) {
-                var request = JSON.parse(rawRequest);
-                requests.push({
-                    fileName: request.fileName,
-                    request: request.request,
-					response: request.response
-                });
-            });
-
-            // set response
-            if (typeof err === 'undefined' || err === null) {
-                res.json(requests);
-            }
-            else {
-                res.statusCode = 500;
-                res.json({ message: 'ups! something went wrong: ' + err });
-            }
-
-        });
-    });
+	this.app.use('/api/requests', requestRoutes);
+	
+	this.app.use('/api', router);
 };
 
 module.exports = AdminServer;
