90650de835a5a896458b3d236f2e83aa5fdb7d1d
begner <ben@egner.it>
2016-09-07 00:08:58 +0200

    New UI Structure, Different CSS for Panel & Popup. CSS Cleanup

--- chrome_plugin/panelCreation/createPanel.js
+++
@@ -1,7 +0,0 @@
-// Can use
-// chrome.devtools.*
-// chrome.extension.*
-
-// Create a tab in the devtools area for managing mocks and requests
-chrome.devtools.panels.create("Manage Mocks", "images/icon.png", "manageMockPanel/manageMockPanel.html", function(panel) {});
-
