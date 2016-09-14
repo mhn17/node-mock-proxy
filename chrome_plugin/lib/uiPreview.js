eef8e343f72fc5281c36fc35dca4c5b09a883e49
Julian Horn <julian.horn@aoe.com>
2016-09-14 14:40:02 +0200

    Adds commentary and makes some parts of the request pane functional again.

--- chrome_plugin/lib/uiPreview.js
+++ chrome_plugin/lib/uiPreview.js
@@ -1,3 +1,4 @@
+// Constructor to init stuff
 var UiPreview = function() {
 
 	this.supportedLanguages = ['xml', 'json'];
@@ -5,11 +6,14 @@ var UiPreview = function() {
 	this.content = '';
 };
 
+// Bind events for buttons
 UiPreview.prototype.bindEvents = function() {
 	var that = this;
 
+	// Unbind old events
 	$(document).unbind('.preview')
 
+	// Format code by displaying the pane anew with a parameter
 	$('#previewWindow #previewLanguage button')
 		.off('click.preview')
 		.on('click.preview', function() {
@@ -17,6 +21,7 @@ UiPreview.prototype.bindEvents = function() {
 			that.show(mode);
 		});
 
+	// Close by X button click
 	$('#previewWindow .closeButton')
 		.off('click.preview')
 		.on('click.preview', function() {
@@ -24,6 +29,7 @@ UiPreview.prototype.bindEvents = function() {
 			that.close();
 		});
 
+	// Close by pressing the escape key
 	$(document).on('keydown', function(event) {
 		if ($('#previewWindow').is(':visible')) {
 			if (event.keyCode === 27) {
@@ -35,9 +41,12 @@ UiPreview.prototype.bindEvents = function() {
 
 };
 
+// Try to autodetect language
 UiPreview.prototype.autoDetectLanguage = function() {
 	var language = null;
 	var autoDetect = hljs.highlightAuto(this.content);
+
+	// Try to find the best language else use the second best identified by the 3rd party library
 	if ($.inArray(autoDetect.language, this.supportedLanguages) !== -1) {
 		language = autoDetect.language;
 	}
@@ -46,6 +55,8 @@ UiPreview.prototype.autoDetectLanguage = function() {
 			language = autoDetect.second_best.language;
 		}
 	}
+
+	// Else give plaintext
 	return language;
 };
 
@@ -54,13 +65,17 @@ UiPreview.prototype.setContent = function(content) {
 	return this;
 };
 
+// Show the popup
+// param language: The language is used to format the code. If no language is given, auto code formating will be tried.
 UiPreview.prototype.show = function(language) {
 
+	// Try to autoformat code if no parameter was given
 	if (typeof(language) === 'undefined') {
 		language = this.autoDetectLanguage();
 	}
 
 	var content = this.content;
+	// Try to format code if a parameter was given
 	try {
 		if (language === 'xml') {
 			content = vkbeautify.xml(content);
@@ -70,6 +85,7 @@ UiPreview.prototype.show = function(language) {
 			content = vkbeautify.json(content);
 		}
 
+		// If no supported language was given, try autoformat.
 		if (language) {
 			content = hljs.highlight(language, content).value;
 			$('#previewWindow').find('code').html(content);
@@ -85,6 +101,7 @@ UiPreview.prototype.show = function(language) {
 	$('#previewWindow').show();
 };
 
-UiPreview.prototype.close = function(language) {
+// Close the popup
+UiPreview.prototype.close = function() {
 	$('#previewWindow').hide();
 };
--- chrome_plugin/manageMockPanel/PaneShowRequests.js
+++ chrome_plugin/manageMockPanel/PaneShowRequests.js
@@ -1,14 +1,38 @@
 var PaneShowRequests = function() {
 
-	this.$container = $('');
+	this.$container = $('#PaneShowRequests');
 	this.apiBridge = new ApiBridge();
 
 	this.bindEvents();
-	this.draw();
 };
 
+// Draw pane
 PaneShowRequests.prototype.draw = function() {
+	var that = this;
+	console.log(this.$container);
+	// Get requests and fill table
+	this.apiBridge.getRequestList(function (requestList) {
+		var $contentTableBody = that.$container.find('tbody');
+		var tableContent = '';
 
+		// Remove old content in order to add data to the already existing data and draw the table anew
+		$contentTableBody.empty();
+
+		// Draw table content
+		requestList.forEach(function (entry) {
+			var rowContent = '';
+			rowContent += '<td><label>' + entry.requestUri + '</label></td>';
+			rowContent += '<td><label>' + entry.method + '</label></td>';
+			rowContent += '<td>';
+			rowContent += '<button>Add</button>';
+			rowContent += '<button>Preview</button>';
+			rowContent += '</td>';
+
+			tableContent += '<tr>' + rowContent + '</tr>';
+		});
+
+		$contentTableBody.append($(tableContent));
+	});
 };
 
 PaneShowRequests.prototype.bindEvents = function() {
