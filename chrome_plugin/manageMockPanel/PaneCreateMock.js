643ca8b8f6a79fffa2ff500f1bb2810c81972d8d
Julian Horn <julian.horn@aoe.com>
2016-09-15 09:19:34 +0200

    Fixes the create mock pane to be able to create mocks again.

--- chrome_plugin/manageMockPanel/PaneCreateMock.js
+++ chrome_plugin/manageMockPanel/PaneCreateMock.js
@@ -1,17 +1,41 @@
+// Constructor to init stuff
 var PaneCreateMock = function() {
 
-	this.$container = $('');
+	this.$container = $('#PaneCreateMock');
+	this.$form = $('#formManuallyCreate');
 	this.apiBridge = new ApiBridge();
 
 	this.bindEvents();
-	this.draw();
 };
 
+// Reset the form to its empty state
 PaneCreateMock.prototype.draw = function() {
-
+	this.$form.trigger('reset');
 };
 
+// Bind events to send the request to the server
 PaneCreateMock.prototype.bindEvents = function() {
 
-};
+	var that = this;
 
+	// Jquery serialize does not create the data as we want it, grml
+	this.$form.submit(function (event) {
+		event.preventDefault();
+		var data = {
+			'id': document.getElementById('form_id').value,
+			'name': document.getElementById('form_name').value,
+			'description': document.getElementById('form_description').value,
+			'requestUri': document.getElementById('form_requestUri').value,
+			'requestMethod': document.getElementById('form_requestMethod').value,
+			'requestBody': document.getElementById('form_requestBody').value,
+			'responseBody': document.getElementById('form_responseBody').value
+		};
+
+		// Send create/update request to server
+		that.apiBridge.createMock(data, function (response) {
+			console.log('createMock', response);
+			that.draw();
+		});
+	});
+
+};
